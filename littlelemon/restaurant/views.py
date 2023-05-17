21e83299b1b332c4de7a8401dab1eb71aa2032c2
Nicolae Ionascu <nionascu@moldcell.md>
2023-05-17 17:32:27 +0300

    Adding serializers (1)

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateView
+from .models import Booking, Menu
+from .serializers import bookingSerializer, menuSerializer
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class bookingview(APIView):
+    
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = bookingSerializer(items, many=True)
+        return Response(serializer.data)
+
+class menuview(APIView):
+    
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = menuSerializer(items, many=True)
+        return Response(serializer.data)    
